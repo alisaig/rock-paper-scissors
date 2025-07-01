ecc929c1217b380f4934c1b420aad1307bad8368
alisaig <alisa.ign20@gmail.com>
2025-07-02 00:44:34 +0200

    Create getComputerChoice function
    Create function to generate one of 3 playing hands

--- javascript.js
+++ javascript.js
@@ -1,1 +1,14 @@
 console.log("Hello World");
+
+function getComputerChoice() {
+    const number = Math.floor(Math.random() * 3);
+    if (number === 0) {
+        return "rock";
+    }
+    else if (number === 1) {
+        return "paper";
+    }
+    else {
+        return "scissors";
+    }
+}
