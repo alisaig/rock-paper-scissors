8a534f304dddb28717cca6f78ddad0fb52604cfa
alisaig <alisa.ign20@gmail.com>
2025-07-02 01:48:43 +0200

    Add playRound function
    Create function to play a single round of game and
    log result of matchup to console, and add score to
    counter

--- javascript.js
+++ javascript.js
@@ -20,3 +20,47 @@ function getHumanChoice() {
     let choice = prompt("Choose the sign you want to play", "rock / paper /scissors");
     return choice;
 }
+
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice === computerChoice) {
+        console.log("It's a draw!");
+    }
+
+    if (humanChoice === "rock") {
+        if (computerChoice === "paper") {
+            computerScore ++;
+            console.log("YOU LOSE! Too bad, Paper beats Rock");
+        }
+        if (computerChoice === "scissors") {
+            humanScore ++;
+            console.log("YOU WIN! Nice one, Rock beats Scissors");
+        }
+    }
+
+    if (humanChoice === "scissors") {
+        if (computerChoice === "rock") {
+            computerScore ++;
+            console.log("YOU LOSE! Too bad, Rock beats Scissors");
+        }
+        if (computerChoice === "paper") {
+            humanScore ++;
+            console.log("YOU WIN! Nice one, Scissors beat Paper");
+        }
+    }
+
+    if (humanChoice === "paper") {
+        if (computerChoice === "scissors") {
+            computerScore ++;
+            console.log("YOU LOSE! Too bad, Scissors beat Paper");
+        }
+        if (computerChoice === "rock") {
+            humanScore ++;
+            console.log("YOU WIN! Nice one, Paper beats Scissors");
+        }
+    }
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
